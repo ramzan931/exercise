d7387be32b3d8a2d4f6e48e731bdc7475b87825c
ramzan931 <ramzanahmad931@gmail.com>
2023-07-04 14:27:36 +0500

    removed semi colon

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,21 +4,22 @@ describe('login', () => {
         cy.get('.nav > :nth-child(1) > a').should('be.visible')
 
     });
+    // test case 2 : invalid password
     it('should display an error message for invalid login credentials', () => {
         cy.get('[data-qa="login-email"]').type('Ramzan.ahmad@invozone.com');
         cy.get('[data-qa="login-password"]').type('invalidPassword');
         cy.get('[data-qa="login-button"]').click();
     });
-
+//test case 3: valid password
     it('should successfully log in with valid credentials', () => {
         cy.get('[data-qa="login-email"]').type('Ramzan.ahmad@invozone.com');
         cy.get('[data-qa="login-password"]').type('123456');
         cy.get('[data-qa="login-button"]').click();
     });
-
+// test case :4 Logout user
     it('should log out from the session', () => {
         cy.get('.shop-menu > .nav > :nth-child(4) > a').click();
     });
 
 
-});
+})
