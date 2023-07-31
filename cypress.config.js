a383b62688698ad0e34d4b8546aa73bb92314e31
ramzan931 <ramzanahmad931@gmail.com>
2023-07-31 15:06:13 +0500

    modify cypress config.js file

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,11 @@ const { defineConfig } = require("cypress");
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 module.exports = defineConfig({
   e2e: {
+    env:{
+      baseUrl: 'https://gorest.co.in/public/v2',
+      token: 'Bearer 4cef239b61082cbbeaa3def254ef32cb975b3985dd0841e9d3ffda4128d5f003'
+
+    },
     setupNodeEvents(on, config) {
       // on('file:preprocessor', webpackPreprocessor);
       allureWriter(on, config);
